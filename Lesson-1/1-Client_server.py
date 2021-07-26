87dbefd1b1c6373618351d0ed8e3a4e39425ac9c
Oluwadahunsi <tommydate0@gmail.com>
2021-07-26 19:31:37 +0300

    first task

---
+++ Lesson-1/1-Client_server.py
@@ -0,0 +1,20 @@
+import requests
+
+def getRepo(USERNAME):
+    header = {
+    "User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    }
+    url = f'https://api.github.com/users/{USERNAME}/repos'
+    response = requests.get(url, headers = header)
+    result = response.json()
+
+    # Save result in json
+    with open('see.json', 'w') as outfile:
+        outfile.write(f'{result}')
+    return result
+
+
+
+
+
+        
